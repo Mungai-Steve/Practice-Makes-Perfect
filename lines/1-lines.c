b5aa2e35394c279e459f11575de450584112fc28
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-25 03:35:00 -0700

    c draw a line of length n where n is equivalent to -

---
+++ lines/1-lines.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * return: program ends here
+ */
+
+int main(void)
+
+{
+	int length, n;
+
+	printf("Enter the length of line:");
+	scanf("%d", &length);
+	if (n <= 0)	
+	for (n = 0; n < length; n++)
+	{
+	putchar(95);
+	}
+	
+	putchar('\n');
+	return (0);
+	
+}
